92baebb9c1b9b37f9f86ff78cfed2e7cbf41b809
Chris Alley <chris@chrisalley.info>
2015-10-23 00:00:43 +1100

    Add Semantic UI modal for illustrations

--- app/assets/javascripts/illustrations.js
+++ app/assets/javascripts/illustrations.js
@@ -5,18 +5,15 @@ $(document).on('ready page:load', function() {
     event.preventDefault();
     popover_src = $(this).children("img").attr("data-popover-src");
     popover_caption = $(this).children("img").attr("alt");
-    popover_width = $(this).children("img").attr("data-popover-width");
-    popover_height = $(this).children("img").attr("data-popover-height");
+    // popover_width = $(this).children("img").attr("data-popover-width");
+    // popover_height = $(this).children("img").attr("data-popover-height");
     $("div#popover_illustration img").attr("src", popover_src);
     $("div#popover_illustration img").attr("alt", popover_caption);
-    $("div#popover_illustration img").attr("width", popover_width);
-    $("div#popover_illustration img").attr("height", popover_height);
+    // $("div#popover_illustration img").attr("width", popover_width);
+    // $("div#popover_illustration img").attr("height", popover_height);
     $("div#popover_illustration p").text(popover_caption);
-    $("div#popover_illustration").fadeIn("fast");
-  });
 
-  $("div#popover_illustration button").click(function() {
-    $("div#popover_illustration").fadeOut("fast");
+    $('#popover_illustration').modal('show');
   });
 
 });
