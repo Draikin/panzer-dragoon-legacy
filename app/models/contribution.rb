f223aa11ef6a9076946fd7047aa75b2a60cf2eb0
Chris Alley <chris@chrisalley.info>
2017-10-19 20:54:39 +1100

    Prevent validation of contributable records when forms containing contributor profiles are saved

--- app/models/contribution.rb
+++ app/models/contribution.rb
@@ -1,4 +1,4 @@
 class Contribution < ApplicationRecord
   belongs_to :contributor_profile
-  belongs_to :contributable, polymorphic: true
+  belongs_to :contributable, polymorphic: true, optional: true
 end
