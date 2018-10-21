390df69b0e9e9a267ad8b8180ce21aee9d110b9e
Chris Alley <chris@chrisalley.info>
2018-10-21 14:16:38 +1300

    Set up redesign namespace and home controller

--- config/routes.rb
+++ config/routes.rb
@@ -35,6 +35,10 @@ Rails.application.routes.draw do
     resources :videos
   end
 
+  namespace :redesign do
+    get '/', to: 'home#index'
+  end
+
   resources :searches
   resources :news_entries, path: 'news'
   get 'site-map', to: 'site_map#show', as: :site_map
