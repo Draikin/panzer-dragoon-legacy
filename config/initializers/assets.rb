8268e7e56acfef52389d28f6c66159e47beb37d5
Chris Alley <chris@chrisalley.info>
2018-10-21 14:34:43 +1300

    Add specific CSS and JavaScript to the redesign

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -7,4 +7,5 @@ Rails.application.config.assets.version = '1.0'
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
-Rails.application.config.assets.precompile += %w( admin.js admin.css.scss )
+Rails.application.config.assets.precompile +=
+  %w( admin.js admin.css.scss redesign.js redesign.css.scss )
